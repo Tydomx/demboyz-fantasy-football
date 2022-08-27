1baf04aedb3587ef2c86879570075741677fd165
Tydomx <michaelthai16@gmail.com>
2022-08-27 12:59:33 -0500

    Getting stuff on the homepage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
+import React, { useState } from 'react';
 import './App.css';
 
 // import components
 import Nav from './assets/components/Nav';
 
+// import pages
+import Home from './assets/components/pages/Home';
+
 function App() {
+  const [currentPage, setCurrentPage] = useState('home');
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'home':
+        return <Home />
+      default:
+        return <Home />
+    }
+  }
+
   return (
     <div>
-      <Nav></Nav>
+      <Nav
+        currentPage={currentPage}
+        changePage={setCurrentPage}
+      />
+      <main>
+        {renderPage()}
+      </main>
     </div>
   );
 }
---
+++ src/assets/components/pages/Home.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default function Home() {
+	return (
+		<section>
+			{/* opening paragraph */}
+			<div>
+				<h1>Season 10 - General Paton joins the fight!</h1>
+				<p>
+					From bottom feeder to title contender all are welcome, but only one will prevail. Many seasons passed in the "Demboyz" fantasy football league and now it's time for the start of a new season... Season 10 of fantasy football!  Time for the raging and broken friendships to ensue!
+				</p>
+			</div>
+
+			<div>
+				<h2>SHAHREZ RANTS</h2>
+				<p>
+					HOW THE FUCK DID JOSH WIN?!?!?! WHAT THE FUCK IS WRONG WITH US? HOW DID WE LET THIS HAPPEN?!? TONY. SOME FANTASY GOD YOU TURNED OUT TO BE. COULDNT EVEN BEAT THE GUY WHO BENCHED KAMARA. BRYAN, WAY TO GO. KICKED ASS ALL SEASON ONLY TO DISAPPEAR WHEN YOU PLAYED AGAINST THE GUY WHO PROBABLY THINKS AFRICA IS A COUNTRY. JAMES ALL THIS TALK ABOUT REDEMPTION AND YOU LET TONY AND THE DICKLESS COWBOYS WASTE YOUR SORRY ASS. FUCK ALL YALL
+
+
+					-Following the 2019 Championship match
+				</p>
+			</div>
+
+			<div>
+				<h2>STUDS AND DUDS</h2>
+				<table>
+					<tr>TBD</tr>
+				</table>
+			</div>
+		</section>
+	)
+}
+
